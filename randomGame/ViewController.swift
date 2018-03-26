85fee68c93918e37e0c0eb7aec7f77c978638691
D7703_06 <d7703_06@D7703-06ui-iMac.local>
2018-03-26 14:51:15 +0900

    Add UITextFieldDelegate

--- randomGame/ViewController.swift
+++ randomGame/ViewController.swift
@@ -8,18 +8,42 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITextFieldDelegate{
+    @IBOutlet weak var randomText: UITextField!
+    @IBOutlet weak var result: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        randomText.delegate = self
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func random(_ sender: UIButton) {
+        let num = arc4random_uniform(6)
+        print(num)
+        if randomText.text == String(num) {
+            result.text = "성공"
+           
+        }
+        else{
+            result.text = "실패. 답은 \(num)"
+            
+        }
+       randomText.resignFirstResponder();
     }
-
-
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool
+    {
+        let num = arc4random_uniform(6)
+        print(num)
+        if randomText.text == String(num) {
+            result.text = "성공"
+            
+        }
+        else{
+            result.text = "실패. 답은 \(num)"
+        }
+        randomText.resignFirstResponder();
+        return true
+    }
+    
 }
 
